3cab0d29b57931bde6e6dc1c84817ed0e12e1794
unknown <dmanning23@hotmail.com>
2013-04-22 21:34:33 -0500

    android poroj

---
+++ Android/Resources/Resource.designer.cs
@@ -0,0 +1,27 @@
+namespace HadoukInput.Android
+{
+	public partial class Resource
+	{
+
+		public partial class Attribute
+		{
+
+			private Attribute()
+			{
+			}
+		}
+
+		public partial class Drawable
+		{
+
+			// aapt resource value: 0x7f020000
+			public const int icon = 2130837504;
+
+			private Drawable()
+			{
+			}
+		}
+		
+	}
+}
+
