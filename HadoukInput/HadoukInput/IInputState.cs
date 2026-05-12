a01248a5b53e503871478627c06985a59a0fe9dd
Dan Manning <1888089+dmanning23@users.noreply.github.com>
2026-05-12 15:17:13 -0500

    Added keyboard state to interface

--- HadoukInput/HadoukInput/IInputState.cs
+++ HadoukInput/HadoukInput/IInputState.cs
@@ -3,37 +3,39 @@ using Microsoft.Xna.Framework.Input;
 
 namespace HadoukInput
 {
-	/// <summary>
-	/// Helper for reading input from keyboard and gamepad. This class tracks both
-	/// the current and previous state of both input devices, and implements query
-	/// methods for high level input actions such as "move up through the menu"
-	/// or "pause the game".
-	/// </summary>
-	public interface IInputState
-	{
-		bool IsConnected(int controllerIndex);
+    /// <summary>
+    /// Helper for reading input from keyboard and gamepad. This class tracks both
+    /// the current and previous state of both input devices, and implements query
+    /// methods for high level input actions such as "move up through the menu"
+    /// or "pause the game".
+    /// </summary>
+    public interface IInputState
+    {
+        bool IsConnected(int controllerIndex);
 
-		bool CheckControllers { get; set; }
+        bool CheckControllers { get; set; }
 
-		float DeadZone { get; }
-		float DeadZoneSquared { get; }
+        float DeadZone { get; }
+        float DeadZoneSquared { get; }
 
-		Vector2 LeftThumbstick(int controllerIndex);
-		Vector2 RightThumbstick(int controllerIndex);
+        Vector2 LeftThumbstick(int controllerIndex);
+        Vector2 RightThumbstick(int controllerIndex);
 
-		KeyboardState CurrentKeyboardState { get; }
+        KeyboardState CurrentKeyboardState { get; }
 
-		KeyboardState LastKeyboardState { get; }
+        KeyboardState LastKeyboardState { get; }
 
-		bool DPadUp(int controllerIndex);
-		bool DPadDown(int controllerIndex);
-		bool DPadLeft(int controllerIndex);
-		bool DPadRight(int controllerIndex);
+        bool DPadUp(int controllerIndex);
+        bool DPadDown(int controllerIndex);
+        bool DPadLeft(int controllerIndex);
+        bool DPadRight(int controllerIndex);
 
-		void Update();
+        void Update();
 
-		bool ButtonDown(int playerIndex, Buttons button);
+        bool IsNewKeyPress(Keys key);
 
-		bool PrevButtonDown(int playerIndex, Buttons button);
-	}
+        bool ButtonDown(int playerIndex, Buttons button);
+
+        bool PrevButtonDown(int playerIndex, Buttons button);
+    }
 }
